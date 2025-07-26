1e571fb3075620284b34d6f3c756450ca66dc94e
IntropeeDev <intropeedev@gmail.com>
2025-07-26 23:59:53 +0530

    Build Pricing Page - II

--- src/landing_page/pricing/Brokerage.jsx
+++ src/landing_page/pricing/Brokerage.jsx
@@ -1,5 +1,100 @@
 function Brokerage() {
-  return <h1>Brokerage</h1>;
+  return (
+    <div className="container mx-auto my-5">
+      <div className="row justify-end my-5">
+        <div className="col-1"></div>
+        <div className="col-10">
+          <h4>Charges for account opening</h4>
+          <table className="table table-striped table-bordered text-[14px] table-sm mt-4">
+            <thead className="fw-300 text-muted">
+              <tr>
+                <th className="text-muted">
+                  <td>Type of account</td>
+                </th>
+                <th className="text-muted">
+                  <td>Charges</td>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td scope="row" className="p-4 mt-3">
+                  Online account
+                </td>
+                <td className="mt-3">free</td>
+              </tr>
+
+              <tr>
+                <td scope="row">Offline account</td>
+                <td className="mt-3">free</td>
+              </tr>
+
+              <tr className="text-muted">
+                <td scope="row" className="pt-3 pl-3">
+                  NRI account (offline only)
+                </td>
+                <td className="w-[15%] text-muted">₹ 500</td>
+              </tr>
+
+              <tr>
+                <td scope="row">
+                  Partnership, LLP, HUF, or Corporate accounts (offline only)
+                </td>
+                <td className="w-[15%] text-muted">₹ 500</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="col-1"></div>
+      </div>
+      <div className="row justify-end">
+        <div className="col-1"></div>
+        <div className="col-10">
+          <h4>Charges for optional value added services</h4>
+          <table className="table table-striped table-bordered text-[14px] table-sm mt-4">
+            <thead className="fw-300 text-muted">
+              <tr>
+                <th className="text-muted">
+                  <td>Type of account</td>
+                </th>
+                <th className="text-muted">
+                  <td>Charges</td>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td scope="row" className="p-4 mt-3">
+                  Online account
+                </td>
+                <td className="mt-3">free</td>
+              </tr>
+
+              <tr>
+                <td scope="row">Offline account</td>
+                <td className="mt-3">free</td>
+              </tr>
+
+              <tr className="text-muted">
+                <td scope="row" className="pt-3 pl-3">
+                  NRI account (offline only)
+                </td>
+                <td className="w-[15%] text-muted">₹ 500</td>
+              </tr>
+
+              <tr>
+                <td scope="row">
+                  Partnership, LLP, HUF, or Corporate accounts (offline only)
+                </td>
+                <td className="w-[15%] text-muted">₹ 500</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="col-1"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Brokerage;
--- src/landing_page/pricing/Charges.jsx
+++ src/landing_page/pricing/Charges.jsx
@@ -1,60 +1,84 @@
 function Charges() {
   return (
     <div className="container mt-5">
-      <h5>
+      <div></div>
+      <h5 className="text-center">
         <a href="#">Calculate your costs upfront </a>
         using our brokerage calculator
       </h5>
-      <h4>Charges explained</h4>
+
       <div className="d-flex row text-muted mx-auto">
-        <div className="col-6 py-5 pl-5">
+        <div className="col-1"></div>
+        <div className="col-5 p-3">
+          <h4>Charges explained</h4>
           <div className="text-muted text-[12px] lh-lg mt-5">
-            <p className="text-[12px]">
+            <div className="text-[12px]">
               <h6 className="text-[16px] mb-3">
-                Securities/Commodities transaction tax{" "}
+                Securities/Commodities transaction tax
               </h6>
-              Tax by the government when transacting on the exchanges. Charged
-              as above on both buy <br />
-              and sell sides when trading equity delivery. Charged only on
-              selling side when trading <br />
-              intraday or on F&O.
+              <p>
+                Tax by the government when transacting on the exchanges. Charged
+                as above on both buy <br />
+                and sell sides when trading equity delivery. Charged only on
+                selling side when trading <br />
+                intraday or on F&O.
+              </p>
+
               <p className="mt-2">
                 When trading at Zerodha, STT/CTT can be a lot more than the
                 brokerage we charge. <br /> Important to keep a tab.
               </p>
-            </p>
-            <p className="text-[12px]">
+            </div>
+            <div className="text-[12px]">
               <h6 className="text-[16px] mb-3">Transaction/Turnover Charges</h6>
-              Charged by exchanges (NSE, BSE, MCX) on the value of your
-              transactions. <br /> BSE has revised transaction charges in XC,
-              XD, XT, Z and ZP groups to ₹10,000 per crore <br />
-              w.e.f 01.01.2016. (XC and XD groups have been merged into a new
-              group X w.e.f 01.12.2017) B <br />
-              SE has revised transaction charges in SS and ST groups to
-              ₹1,00,000 per crore of gross <br />
-              turnover. <br /> BSE has revised transaction charges for group A,
-              B and other non exclusive scrips (non- <br />
-              exclusive scrips from group E, F, FC, G, GC, W, T) at ₹375 per
-              crore of turnover on flat rate <br /> basis w.e.f. December 1,
-              2022.
-              <br />
-              BSE has revised transaction charges in M, MT, TS and MS groups to
-              ₹275 per crore of gross turnover.
-            </p>
-            <p className="text-[12px]">
+              <p>
+                Charged by exchanges (NSE, BSE, MCX) on the value of your
+                transactions.
+              </p>
+              <p>
+                BSE has revised transaction charges in XC, XD, XT, Z and ZP
+                groups to ₹10,000 per crore <br />
+                w.e.f 01.01.2016. (XC and XD groups have been merged into a new
+                group X w.e.f 01.12.2017) B <br />
+              </p>
+              <p>
+                SE has revised transaction charges in SS and ST groups to
+                ₹1,00,000 per crore of gross <br />
+                turnover. <br />
+              </p>
+              <p>
+                BSE has revised transaction charges for group A, B and other non
+                exclusive scrips (non- <br />
+                exclusive scrips from group E, F, FC, G, GC, W, T) at ₹375 per
+                crore of turnover on flat rate <br /> basis w.e.f. December 1,
+                2022.
+                <br />
+              </p>
+              <p>
+                BSE has revised transaction charges in M, MT, TS and MS groups
+                to ₹275 per crore of gross <br />
+                turnover.
+              </p>
+            </div>
+            <div className="text-[12px]">
               <h6 className="text-[16px] mb-3">Call & trade</h6>
-              Additional charges of ₹50 per order for orders placed through a
-              dealer at Zerodha including <br />
-              auto square off orders.
-            </p>
-            <p className="text-[12px]">
+              <p>
+                Additional charges of ₹50 per order for orders placed through a
+                dealer at Zerodha including <br />
+                auto square off orders.
+              </p>
+            </div>
+            <div className="text-[12px]">
               <h6 className="text-[16px] mb-3">Stamp charges </h6>
-              Stamp charges by the Government of India as per the Indian Stamp
-              Act of 1899 for <br />
-              transacting in instruments on the stock exchanges and depositories
-            </p>
+              <p>
+                Stamp charges by the Government of India as per the Indian Stamp
+                Act of 1899 for <br />
+                transacting in instruments on the stock exchanges and
+                depositories
+              </p>
+            </div>
 
-            <p className="text-[12px]">
+            <div className="text-[12px]">
               <h6 className="text-[16px] mb-3">NRI brokerage charges</h6>
               <div className="ml-3">
                 <li>₹100 per order for futures and options.</li>
@@ -71,7 +95,7 @@ function Charges() {
                   charges.
                 </li>
               </div>
-            </p>
+            </div>
             <p className="text-[12px]">
               <h6 className="text-[16px] mb-3">Account with debit balance</h6>
               If the account is in debit balance, any order placed will be
@@ -91,7 +115,8 @@ function Charges() {
                 </li>
                 <li>
                   Currency - ₹0.05 per lakh + GST of turnover for Futures and ₹2
-                  per lakh + GST of <br /> premium for Options.
+                  per lakh + GST of <br /> &nbsp; &nbsp; &nbsp;premium for
+                  Options.
                 </li>
               </div>
             </p>
@@ -117,104 +142,123 @@ function Charges() {
             </p>
           </div>
         </div>
-        <div className="col-6 py-5 pl-5 ">
+        <div className="col-5 py-5 ">
           <div className="text-muted text-[12px] lh-lg  mt-5">
             <p className="text-[12px]">
-              <h6 className="text-[16px] mb-3">
-                Securities/Commodities transaction tax{" "}
-              </h6>
-              Tax by the government when transacting on the exchanges. Charged
-              as above on both buy and sell sides when trading equity delivery.
-              Charged only on selling side when trading intraday or on F&O.{" "}
-              <br /> When trading at Zerodha, STT/CTT can be a lot more than the
-              brokerage we charge. Important to keep a tab.
+              <h6 className="text-[16px] mb-3">GST</h6>
+              Tax levied by the government on the services rendered. 18% of (
+              brokerage + SEBI charges <br /> + transaction charges)
             </p>
             <p className="text-[12px]">
-              <h6 className="text-[16px] mb-3">Transaction/Turnover Charges</h6>
-              Charged by exchanges (NSE, BSE, MCX) on the value of your
-              transactions. <br /> BSE has revised transaction charges in XC,
-              XD, XT, Z and ZP groups to ₹10,000 per crore w.e.f 01.01.2016. (XC
-              and XD groups have been merged into a new group X w.e.f
-              01.12.2017) B <br />
-              SE has revised transaction charges in SS and ST groups to
-              ₹1,00,000 per crore of gross turnover. <br /> BSE has revised
-              transaction charges for group A, B and other non exclusive scrips
-              (non-exclusive scrips from group E, F, FC, G, GC, W, T) at ₹375
-              per crore of turnover on flat rate basis w.e.f. December 1, 2022.
+              <h6 className="text-[16px] mb-3">SEBI Charges</h6>
+              Charged at ₹10 per crore + GST by Securities and Exchange Board of
+              India for regulating the <br />
+              markets.
+            </p>
+            <div className="text-[12px]">
+              <h6 className="text-[16px] mb-3">
+                DP (Depository participant) charges
+              </h6>
+              <p>
+                ₹15.34 per scrip (₹3.5 CDSL fee + ₹9.5 Zerodha fee + ₹2.34 GST)
+                is charged on the trading <br />
+                account ledger when stocks are sold, irrespective of quantity.
+              </p>
+              <p>
+                Female demat account holders (as first holder) will enjoy a
+                discount of ₹0.25 per transaction <br />
+                on the CDSL fee.
+              </p>
+              <p>
+                Debit transactions of mutual funds & bonds get an additional
+                discount of ₹0.25 on the CDSL <br />
+                fee.
+              </p>
+            </div>
+            <p className="text-[12px]">
+              <h6 className="text-[16px] mb-3">Pledging charges</h6>
+              ₹30 + GST per pledge request per ISIN.
+            </p>
+            <p className="text-[12px]">
+              <h6 className="text-[16px] mb-3">
+                AMC (Account maintenance charges)
+              </h6>
+              For BSDA demat account: Zero charges if the holding value is less
+              than ₹4,00,000. To learn <br />
+              more about BSDA, <a href="#">Click here</a>
+              <p>
+                For non-BSDA demat accounts: ₹300/year + 18% GST charged
+                quarterly (90 days). To learn
+                <br /> more about AMC, <a href="#">Click here</a>
+              </p>
+            </p>
+            <p className="text-[12px]">
+              <h6 className="text-[16px] mb-3">
+                Corporate action order charges
+              </h6>
+              ₹20 plus GST will be charged for OFS / buyback / takeover /
+              delisting orders placed through
+              <br /> Console.
+            </p>
+            <p className="text-[12px]">
+              <h6 className="text-[16px] mb-3">Off-market transfer charges</h6>
+              ₹25 per transaction.
+            </p>
+            <p className="text-[12px]">
+              <h6 className="text-[16px] mb-3">Physical CMR request</h6>
+              First CMR request is free. ₹20 + ₹100 (courier charge) + 18% GST
+              for subsequent requests.
+            </p>
+            <p className="text-[12px]">
+              <h6 className="text-[16px] mb-3">Payment gateway charges</h6>
+              ₹9 + GST (Not levied on transfers done via UPI)
+            </p>
+            <p className="text-[12px]">
+              <h6 className="text-[16px] mb-3">Delayed Payment Charges</h6>
+              Interest is levied at 18% a year or 0.05% per day on the debit
+              balance in your trading <br /> account.
+              <a href="#"> Learn more.</a>
+            </p>
+            <div className="text-[12px]">
+              <h6 className="text-[16px] mb-3">
+                Trading using 3-in-1 account with block functionality
+              </h6>
+              <div className="ml-2">
+                <li>Delivery & MTF Brokerage: 0.5% per executed order.</li>
+                <li>Intraday Brokerage: 0.05% per executed order.</li>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-1"></div>
+      <div className="row mb-5 p-4">
+        <div className="col-1"></div>
+        <div className="col-10">
+          <div className="text-[12px] lh-lg">
+            <h6 className="text-[16px] mb-3">Disclaimer</h6>
+            <p>
+              For Delivery based trades, a minimum of ₹0.01 will be charged per
+              contract note. Clients who opt to receive physical contract notes
+              will be charged ₹20 per contract note plus courier charges. <br />
+              Brokerage will not exceed the rates specified by SEBI and the
+              exchanges. All statutory and regulatory charges will be levied at
+              actuals. Brokerage is also charged on expired, exercised, and{" "}
               <br />
-              BSE has revised transaction charges in M, MT, TS and MS groups to
-              ₹275 per crore of gross turnover.
-            </p>
-            <p className="text-[12px]">
-              <h6 className="text-[16px] mb-3">Call & trade</h6>
-              Additional charges of ₹50 per order for orders placed through a
-              dealer at Zerodha including auto square off orders.
-            </p>
-            <p className="text-[12px]">
-              <h6 className="text-[16px] mb-3">Stamp charges </h6>
-              Stamp charges by the Government of India as per the Indian Stamp
-              Act of 1899 for transacting in instruments on the stock exchanges
-              and depositories
-            </p>
-            <p className="text-[12px]">
-              <h6 className="text-[16px] mb-3">NRI brokerage charges</h6>
-              <ul>
-                <li>₹100 per order for futures and options.</li>
-                <li>
-                  For a non-PIS account, 0.5% or ₹100 per executed order for
-                  equity (whichever is lower).
-                </li>
-                <li>
-                  {" "}
-                  For a PIS account, 0.5% or ₹200 per executed order for equity
-                  (whichever is lower).
-                </li>
-                <li>
-                  ₹500 + GST as yearly account maintenance charges (AMC)
-                  charges.
-                </li>
-              </ul>
-            </p>
-            <p className="text-[12px]">
-              <h6 className="text-[16px] mb-3">Account with debit balance</h6>
-              If the account is in debit balance, any order placed will be
-              charged ₹40 per executed order instead of ₹20 per executed order.
-            </p>
-            <p className="text-[12px]">
-              <h6 className="text-[16px] mb-3">
-                Charges for Investor's Protection Fund Trust (IPFT) by NSE
-              </h6>
-              <li>
-                Equity and Futures - ₹10 per crore + GST of the traded value.
-              </li>
-              <li>
-                Options - ₹50 per crore + GST traded value (premium value).
-              </li>
-              <li>
-                Currency - ₹0.05 per lakh + GST of turnover for Futures and ₹2
-                per lakh + GST of premium for Options.
-              </li>
-            </p>
-            <p className="text-[12px]">
-              <h6 className="text-[16px] mb-3">
-                Margin Trading Facility (MTF)
-              </h6>
-              <li>
-                MTF Interest: 0.04% per day (₹40 per lakh) on the funded amount.
-                The interest is applied from T+1 day until the day MTF stocks
-                are sold.
-              </li>
-              <li>
-                MTF Brokerage: 0.3% or Rs. 20/executed order, whichever is
-                lower.
-              </li>
-              <li>
-                MTF pledge charge: ₹15 + GST per pledge and unpledge request per
-                ISIN
-              </li>
+              assigned options contracts. Free investments are available only
+              for our retail individual clients. Companies, Partnerships,
+              Trusts, and HUFs need to pay 0.1% or ₹20 (whichever is less) as
+              delivery
+              <br />
+              brokerage. A brokerage of 0.25% of the contract value will be
+              charged for contracts where physical delivery happens. For netted
+              off positions in physically settled contracts, a brokerage of 0.1%{" "}
+              <br />
+              will be charged
             </p>
           </div>
         </div>
+        <div className="col-1"></div>
       </div>
     </div>
   );
